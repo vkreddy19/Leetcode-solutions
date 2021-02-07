aed489ebfcce632c516c75dc74e27dc4b7cd3391
vamshi <vkreddy1994@gmail.com>
2021-02-07 02:37:43 -0500

    Alignment

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,18 +1,16 @@
 public class BinarySearch {
     public int search(int[] nums, int target) {
-        int mid = nums.length/2;
-        int l=0, r=nums.length-1;
-        while(l<=r){
-            if(nums[mid] == target){
+        int mid = nums.length / 2;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            if (nums[mid] == target) {
                 return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
-            else if(nums[mid] > target){
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
-            mid = (l+r)/2;
+            mid = (l + r) / 2;
         }
         return -1;
     }
